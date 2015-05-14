7564a12c23c55363021f9cda916ad39414a1d7db
FurkanSalih <furkansalihvarol@gmail.com>
2015-05-14 11:02:49 +0300

    First init

---
+++ histogram_item.py
@@ -0,0 +1,6 @@
+class Histogram_Item(object):
+	"""docstring for Histogram_Item"""
+	def __init__(self, pos, color):
+		self.pos = pos
+		self.color = color
+		
---
+++ kmeans.py
@@ -0,0 +1,14 @@
+
+
+class KMeans(object):
+	"""docstring for KMeans"""
+	def __init__(self, value_space, k=2, done_count):
+		self.value_space = value_space
+		self.k = k
+		self.done_count = done_count
+		
+	def calculate():
+		pass
+
+
+
